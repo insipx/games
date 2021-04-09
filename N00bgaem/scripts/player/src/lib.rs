12bf7b6f0f7299fd8c23fc2cef7a2204006ed420
Andrew Plaza <andrew.plaza@parity.io>
2021-04-10 00:31:44 +0200

    stuff works!

--- N00bgaem/scripts/player/src/lib.rs
+++ N00bgaem/scripts/player/src/lib.rs
@@ -1,19 +1,52 @@
 use gdnative::prelude::*;
 
+const SPEED: f32 = 6.0;
+
 #[derive(NativeClass)]
-#[inherit(Node)]
-struct Player;
+#[inherit(KinematicBody)]
+struct Player {
+    velocity: Vector3,
+}
 
 #[methods]
 impl Player {
-    fn new(_owner: &Node) -> Self {
-        Player
+    fn new(_owner: &KinematicBody) -> Self {
+        Player {
+            velocity: Vector3::new(0.0, 0.0, 0.0)
+        }
     }
     
     #[export]
-    fn _ready(&mut self, mut _owner: &Node) {
+    fn _ready(&mut self, mut _owner: &KinematicBody) {
         godot_print!("What's up, Motherfuckers!!!");
         // owner.set_physics_process(true);
+    }
+   
+    // this runs at constant 60fps
+    #[export]
+    fn _physics_process(&mut self, owner: &KinematicBody, _delta: f64)  {
+        let input = Input::godot_singleton();
+
+        if input.is_action_pressed("ui_right") && input.is_action_pressed("ui_left") {
+            self.velocity.x = 0.0;
+        } else if input.is_action_pressed("ui_right") {
+            self.velocity.x = SPEED;
+        } else if input.is_action_pressed("ui_left") {
+            self.velocity.x = -SPEED;
+        } else {
+            self.velocity = self.velocity.lerp(Vector3::new(0.0, 0.0, self.velocity.z), 0.10);
+        }
+
+        if input.is_action_pressed("ui_up") && input.is_action_pressed("ui_down") {
+            self.velocity.z = 0.0;
+        } else if input.is_action_pressed("ui_up") {
+            self.velocity.z = -SPEED;
+        } else if input.is_action_pressed("ui_down") {
+            self.velocity.z = SPEED;
+        } else {
+            self.velocity = self.velocity.lerp(Vector3::new(self.velocity.x, 0.0, 0.0), 0.1);
+        }
+        owner.move_and_slide(self.velocity, Vector3::default(), true, 10, 45.0, true);
     }
 }
 
